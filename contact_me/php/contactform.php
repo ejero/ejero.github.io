0b14d418b6569c7e45d1f386d69eaa6ced9c14ae
rosita <rositaemakpo@gmail.com>
2018-07-31 12:29:19 -0700

    updating about and contact

---
+++ contact_me/php/contactform.php
@@ -0,0 +1,12 @@
+<?php 
+if(isset($_POST["submit"])){
+    $name = $_POST["name"];
+    $mailFrom = $_POST["email"];
+    $url = $_POST["url"];
+    $message = $_POST["message"];
+    $mailTo = "rosita@quoody.com";
+    $heaaders = "From: ".$mailFrom;
+    $txt = "You have receved an email from ".$name.".\n\n".$message;
+    mail($mailTo, $txt, $heaaders);
+    header("Location: index.php?mailsent");
+}
